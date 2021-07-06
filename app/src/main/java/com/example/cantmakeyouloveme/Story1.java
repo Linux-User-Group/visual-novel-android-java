d5ffe972886c7114c22c9e8324bf3fba2a2129a9
farisqlail <faris.riskilail@gmail.com>
2021-07-06 11:44:18 +0700

    typewriter

--- app/src/main/java/com/example/cantmakeyouloveme/Story1.java
+++ app/src/main/java/com/example/cantmakeyouloveme/Story1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -37,7 +38,13 @@ public class Story1 extends AppCompatActivity {
 
         maleChar1.setAnimation(slideDown);
 
-        txtStory.setText(text);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                txtStory.setText(text);
+            }
+        }, 500);
 
     }
 }
